5c1eb232b9f58c4522d7772e1bbaa3dbedbdaf41
Basant Sharma <basantsharma1990@gmail.com>
2024-12-03 20:51:57 +0200

    updating

--- carla/optimizer/cem.py
+++ carla/optimizer/cem.py
@@ -431,180 +431,7 @@ class CEM():
 
         behavioural_inputs = jnp.hstack((v_des1,v_des2,v_des3,v_des4,y_des1,y_des2,y_des3,y_des4))
 
-        return cx_best,cy_best,mmd_lane,mmd_obs,v_best,steering_best,\
-                    x_roll_global, y_roll_global,mean_param,ego_init_frenet,\
-                    behavioural_inputs, beta_cem_data_best,res
-    
-    @partial(jit, static_argnums=(0, ))	
-    def compute_cem_mmd_random(self,idx_mpc,init_state_global,
-                    mean_param_init,cov_param_init,
-                    x_obs_traj,y_obs_traj,v_des,
-                    x_path,y_path,arc_vec,Fx_dot,Fy_dot,kappa):
-        
-        lamda_x_init = jnp.zeros((self.num_batch, self.nvar))
-        lamda_y_init = jnp.zeros((self.num_batch, self.nvar))
-        s_lane_init = jnp.zeros((self.num_batch, 2*(self.num-1)))
-
-        res_init = jnp.zeros(self.maxiter_cem)
-        res_2_init = jnp.zeros(self.maxiter_cem)
-
-        neural_output_batch_init = self.cem_helper.sampling_param(mean_param_init, cov_param_init)
-
-        x_global_init, y_global_init, v_global_init, vdot_global_init, \
-        psi_global_init, psidot_global_init = init_state_global
-        
-        x_init, y_init, vx_init, vy_init, ax_init, ay_init, psi_init,_,_ = self.cem_helper.global_to_frenet(x_path, y_path,
-                                                                    init_state_global, arc_vec,
-                                                                    Fx_dot, Fy_dot, kappa )
-        
-        b_eq_x, b_eq_y = self.cem_helper.compute_boundary_vec(x_init, vx_init, 
-                                                   ax_init, y_init, vy_init, ay_init)
-        
-        _initial_state_global = jnp.asarray([x_global_init,y_global_init,v_global_init*jnp.cos(psi_global_init),\
-                                        v_global_init*jnp.sin(psi_global_init),psi_global_init])
-
-        beta = (1/self.num_reduced_sqrt)*jnp.ones((self.ellite_num_projection,self.num_reduced_sqrt))
-        sigma = 0.01*jnp.ones(self.ellite_num_projection)
-
-        def lax_cem(carry,idx):
-
-            res,res_2,lamda_x, lamda_y, neural_output_batch,\
-                mean_param,cov_param,s_lane = carry
-
-            key = jax.random.PRNGKey(3*idx_mpc + 5*idx + 7)
-
-            c_x_bar, c_y_bar = self.cem_helper.compute_x_guess(b_eq_x, b_eq_y,
-                                                    neural_output_batch)
-
-            c_x, c_y, x, y, xdot, ydot, xddot, yddot, res_norm_batch,\
-            lamda_x, lamda_y,s_lane,steering,kappa_interp\
-            = self.projection.compute_projection(x_obs_traj,y_obs_traj,b_eq_x, b_eq_y,
-                                                                            lamda_x, lamda_y, c_x_bar, c_y_bar,self.a_obs,self.b_obs,s_lane,
-                                                                            arc_vec, kappa)
-
-            idx_ellite_projection = jnp.argsort(res_norm_batch)
-
-            x_ellite_projection = x[idx_ellite_projection[0:self.ellite_num_projection]]
-            y_ellite_projection = y[idx_ellite_projection[0:self.ellite_num_projection]]
-
-            xdot_ellite_projection = xdot[idx_ellite_projection[0:self.ellite_num_projection]]
-            ydot_ellite_projection = ydot[idx_ellite_projection[0:self.ellite_num_projection]]
-            
-            xddot_ellite_projection = xddot[idx_ellite_projection[0:self.ellite_num_projection]]
-            yddot_ellite_projection = yddot[idx_ellite_projection[0:self.ellite_num_projection]]
-
-            c_x_ellite_projection = c_x[idx_ellite_projection[0:self.ellite_num_projection]]
-            c_y_ellite_projection = c_y[idx_ellite_projection[0:self.ellite_num_projection]]
-            res_ellite_projection = res_norm_batch[idx_ellite_projection[0:self.ellite_num_projection]]
-
-            steering_ellite_projection = steering[idx_ellite_projection[0:self.ellite_num_projection]]
-            kappa_ellite_projection = kappa_interp[idx_ellite_projection[0:self.ellite_num_projection]]
-
-            neural_output_projection = neural_output_batch[idx_ellite_projection[0:self.ellite_num_projection]]
-
-            acc_ellite_projection,_ \
-                = self.cem_helper.compute_controls(xdot_ellite_projection, ydot_ellite_projection,
-                                        xddot_ellite_projection,yddot_ellite_projection)
-            
-            key,subkey = jax.random.split(key)
-
-            x_roll_global_ellite_projection,y_roll_global_ellite_projection\
-                = self.cem_helper.compute_rollout_vmap(acc_ellite_projection[:,0:self.num_prime],
-                                                            steering_ellite_projection[:,0:self.num_prime]
-                                    ,_initial_state_global,key)
-            
-            x_roll_frenet_ellite_projection,y_roll_frenet_ellite_projection \
-                = self.cem_helper.global_to_frenet_vmap(x_roll_global_ellite_projection,y_roll_global_ellite_projection,
-                                             x_path, y_path, arc_vec, Fx_dot, Fy_dot)
-
-            mmd_obs = self.costs.compute_mmd_obs_vmap(beta,sigma,x_roll_frenet_ellite_projection,
-                                                      y_roll_frenet_ellite_projection,
-                                                      x_obs_traj[:,0:self.num_prime],y_obs_traj[:,0:self.num_prime])
-
-            idx_ellite_mmd = jnp.argsort(mmd_obs)
-
-            mmd_obs_ellite = mmd_obs[idx_ellite_mmd[0:self.ellite_num_cost]]
-
-            x_ellite_mmd = x_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            y_ellite_mmd = y_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-
-            xdot_ellite_mmd = xdot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            ydot_ellite_mmd = ydot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            
-            xddot_ellite_mmd = xddot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            yddot_ellite_mmd = yddot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-
-            c_x_ellite_mmd = c_x_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            c_y_ellite_mmd = c_y_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            res_ellite_mmd = res_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-
-            neural_ellite_mmd = neural_output_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-
-            x_roll_frenet_ellite_mmd = x_roll_frenet_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            y_roll_frenet_ellite_mmd = y_roll_frenet_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            steer_ellite_mmd = steering_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            kappa_ellite_mmd = kappa_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            
-            
-            beta_ellite_mmd = beta[idx_ellite_mmd[0:self.ellite_num_cost]]
-            sigma_ellite_mmd = sigma[idx_ellite_mmd[0:self.ellite_num_cost]]
-            
-            x_roll_global_ellite_mmd = x_roll_global_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            y_roll_global_ellite_mmd = y_roll_global_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
-            
-            mmd_lane  = self.costs.compute_mmd_lane_vmap(beta_ellite_mmd,sigma_ellite_mmd,y_roll_frenet_ellite_mmd)
-            mmd_des_lane  = self.costs.compute_lane_des_mmd_vmap(beta_ellite_mmd,sigma_ellite_mmd,y_roll_frenet_ellite_mmd)
-
-            cost_batch = self.cem_helper.compute_cost(self.weight_mmd_lane_des*mmd_des_lane,self.weight_mmd_obs*mmd_obs_ellite,self.weight_mmd_lane*mmd_lane
-                                                      ,x_ellite_mmd,y_ellite_mmd, res_ellite_mmd, \
-                                        xdot_ellite_mmd, ydot_ellite_mmd, 
-                                        xddot_ellite_mmd, yddot_ellite_mmd, \
-                                            v_des, steer_ellite_mmd,kappa_ellite_mmd)
-
-            neural_output_ellite, idx_ellite = self.cem_helper.compute_ellite_samples(cost_batch, neural_ellite_mmd)
-            
-            key, subkey = jax.random.split(key)
-
-            mean_param, cov_param, neural_output_batch, cost_batch_temp\
-                 = self.cem_helper.compute_shifted_samples(key, neural_output_ellite, 
-                cost_batch, idx_ellite, mean_param, cov_param)
-        
-            idx_min = jnp.argmin(cost_batch_temp)
-          
-            res = res.at[idx].set(jnp.min(cost_batch_temp))
-            res_2 = res_2.at[idx].set(res_ellite_mmd[idx_min])
-
-            return (res,res_2,lamda_x, lamda_y, neural_output_batch,
-                    mean_param,cov_param,s_lane),\
-                (c_x_ellite_mmd[idx_min],c_y_ellite_mmd[idx_min],
-                 steer_ellite_mmd[idx_min],
-                 mmd_lane[idx_min],mmd_obs_ellite[idx_min],
-                 x_roll_global_ellite_mmd[idx_min],y_roll_global_ellite_mmd[idx_min],
-                 neural_ellite_mmd[idx_min])
-
-        carry_init = (res_init,res_2_init,lamda_x_init, lamda_y_init, \
-            neural_output_batch_init,mean_param_init, cov_param_init,s_lane_init)
-
-        carry_final,result = lax.scan(lax_cem,carry_init,jnp.arange(self.maxiter_cem))
-
-        res,res_2,lamda_x, lamda_y, neural_output_batch,\
-            mean_param,cov_param,s_lane = carry_final 
-        
-        cx_best = result[0][-1]
-        cy_best = result[1][-1]
-
-        xdot_best = jnp.dot(self.Pdot_jax, cx_best)
-        ydot_best = jnp.dot(self.Pdot_jax, cy_best)
-        v_best = jnp.sqrt(xdot_best**2+ydot_best**2)
-        steering_best = result[2][-1]
-       
-        mmd_lane = result[3][-1]
-        mmd_obs = result[4][-1]
-
-        x_roll_global, y_roll_global = result[5][-1], result[6][-1]
-        
-        return cx_best,cy_best,mmd_lane,mmd_obs,v_best,steering_best,\
-                    x_roll_global, y_roll_global,mean_param,0,0,0
+        return cx_best,cy_best,v_best,steering_best,mean_param
     
     @partial(jit, static_argnums=(0, ))	
     def compute_cem_cvar(self,idx_mpc,init_state_global,
@@ -792,6 +619,169 @@ class CEM():
 
         x_roll_global, y_roll_global = result[5][-1], result[6][-1]
 
-        return cx_best,cy_best,mmd_lane,mmd_obs,v_best,steering_best,\
-                    x_roll_global, y_roll_global,mean_param,0,0,0,0
+        return cx_best,cy_best,v_best,steering_best,mean_param
+
     
+    @partial(jit, static_argnums=(0, ))	
+    def compute_cem_det(self,idx_mpc,init_state_global,
+                    mean_param_init,cov_param_init,
+                    x_obs_traj,y_obs_traj,v_des,
+                    x_path,y_path,arc_vec,Fx_dot,Fy_dot,kappa):
+        
+        lamda_x_init = jnp.zeros((self.num_batch, self.nvar))
+        lamda_y_init = jnp.zeros((self.num_batch, self.nvar))
+        s_lane_init = jnp.zeros((self.num_batch, 2*(self.num-1)))
+
+        res_init = jnp.zeros(self.maxiter_cem)
+        res_2_init = jnp.zeros(self.maxiter_cem)
+
+        neural_output_batch_init = self.cem_helper.sampling_param(mean_param_init, cov_param_init)
+
+        x_global_init, y_global_init, v_global_init, vdot_global_init, \
+        psi_global_init, psidot_global_init = init_state_global
+        
+        ### Noiseless init state
+
+        # x_init, y_init, vx_init, vy_init, ax_init, ay_init, psi_init,_,_ = self.cem_helper.global_to_frenet(x_path, y_path,
+        #                                                             init_state_global, arc_vec,
+        #                                                             Fx_dot, Fy_dot, kappa )
+        
+        # b_eq_x, b_eq_y = self.cem_helper.compute_boundary_vec(x_init, vx_init, 
+        #                                            ax_init, y_init, vy_init, ay_init)
+        
+        # _initial_state_global = jnp.asarray([x_global_init,y_global_init,v_global_init*jnp.cos(psi_global_init),\
+        #                                 v_global_init*jnp.sin(psi_global_init),psi_global_init])
+
+        #### Noisy init state
+
+        x_init,y_init,vx_init,vy_init = x_global_init,y_global_init,v_global_init*jnp.cos(psi_global_init),\
+                                        v_global_init*jnp.sin(psi_global_init)
+        ax_init,ay_init = 0.,0.
+        x_init,y_init,vx_init,vy_init,psi_init = self.cem_helper.compute_noisy_init_state(idx_mpc,x_init,y_init,vx_init,vy_init)
+       
+        temp = jnp.asarray([x_init,y_init, jnp.sqrt(vx_init**2+vy_init**2), init_state_global[3].reshape(-1),
+                             psi_init.reshape(-1), init_state_global[5].reshape(-1)])
+        
+        x_init, y_init, vx_init, vy_init, ax_init, ay_init, _,_,_ = self.cem_helper.global_to_frenet(x_path, y_path,
+                                                                                             temp, arc_vec,
+                                                                                              Fx_dot, Fy_dot, kappa )
+        
+        b_eq_x, b_eq_y = self.cem_helper.compute_boundary_vec(x_init, vx_init, 
+                                                   ax_init, y_init, vy_init, ay_init)
+        
+        def lax_cem(carry,idx):
+
+            res,res_2,lamda_x, lamda_y, neural_output_batch,\
+                mean_param,cov_param,s_lane = carry
+
+            key = jax.random.PRNGKey(3*idx_mpc + 5*idx + 7)
+
+            c_x_bar, c_y_bar = self.cem_helper.compute_x_guess(b_eq_x, b_eq_y,
+                                                    neural_output_batch)
+
+            c_x, c_y, x, y, xdot, ydot, xddot, yddot, res_norm_batch,\
+            lamda_x, lamda_y,s_lane,steering,kappa_interp\
+            = self.projection.compute_projection(x_obs_traj,y_obs_traj,b_eq_x, b_eq_y,
+                                                                            lamda_x, lamda_y, c_x_bar, c_y_bar,self.a_obs,self.b_obs,s_lane,
+                                                                            arc_vec, kappa)
+
+            idx_ellite_projection = jnp.argsort(res_norm_batch)
+
+            x_ellite_projection = x[idx_ellite_projection[0:self.ellite_num_projection]]
+            y_ellite_projection = y[idx_ellite_projection[0:self.ellite_num_projection]]
+
+            xdot_ellite_projection = xdot[idx_ellite_projection[0:self.ellite_num_projection]]
+            ydot_ellite_projection = ydot[idx_ellite_projection[0:self.ellite_num_projection]]
+            
+            xddot_ellite_projection = xddot[idx_ellite_projection[0:self.ellite_num_projection]]
+            yddot_ellite_projection = yddot[idx_ellite_projection[0:self.ellite_num_projection]]
+
+            c_x_ellite_projection = c_x[idx_ellite_projection[0:self.ellite_num_projection]]
+            c_y_ellite_projection = c_y[idx_ellite_projection[0:self.ellite_num_projection]]
+            res_ellite_projection = res_norm_batch[idx_ellite_projection[0:self.ellite_num_projection]]
+
+            steering_ellite_projection = steering[idx_ellite_projection[0:self.ellite_num_projection]]
+            kappa_ellite_projection = kappa_interp[idx_ellite_projection[0:self.ellite_num_projection]]
+
+            neural_output_projection = neural_output_batch[idx_ellite_projection[0:self.ellite_num_projection]]
+
+            acc_ellite_projection,_ \
+                = self.cem_helper.compute_controls(xdot_ellite_projection, ydot_ellite_projection,
+                                        xddot_ellite_projection,yddot_ellite_projection)
+            
+            key,subkey = jax.random.split(key)
+            
+            x_roll_frenet_ellite_projection,y_roll_frenet_ellite_projection \
+                = x_ellite_projection,y_ellite_projection
+
+            mmd_obs = jnp.zeros((self.ellite_num_projection))
+
+            idx_ellite_mmd = jnp.argsort(mmd_obs)
+
+            mmd_obs_ellite = mmd_obs[idx_ellite_mmd[0:self.ellite_num_cost]]
+
+            x_ellite_mmd = x_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            y_ellite_mmd = y_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+
+            xdot_ellite_mmd = xdot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            ydot_ellite_mmd = ydot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            
+            xddot_ellite_mmd = xddot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            yddot_ellite_mmd = yddot_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+
+            c_x_ellite_mmd = c_x_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            c_y_ellite_mmd = c_y_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            res_ellite_mmd = res_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+
+            neural_ellite_mmd = neural_output_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+
+            x_roll_frenet_ellite_mmd = x_roll_frenet_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            y_roll_frenet_ellite_mmd = y_roll_frenet_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            steer_ellite_mmd = steering_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+            kappa_ellite_mmd = kappa_ellite_projection[idx_ellite_mmd[0:self.ellite_num_cost]]
+           
+            
+            mmd_lane  = jnp.zeros((self.ellite_num_cost))
+            mmd_des_lane  = jnp.zeros((self.ellite_num_cost))
+
+            cost_batch = self.cem_helper.compute_cost(0*mmd_des_lane,0*mmd_obs_ellite,0*mmd_lane
+                                                      ,x_ellite_mmd,y_ellite_mmd, res_ellite_mmd, \
+                                        xdot_ellite_mmd, ydot_ellite_mmd, 
+                                        xddot_ellite_mmd, yddot_ellite_mmd, \
+                                            v_des, steer_ellite_mmd,kappa_ellite_mmd)
+
+            neural_output_ellite, idx_ellite = self.cem_helper.compute_ellite_samples(cost_batch, neural_ellite_mmd)
+            
+            key, subkey = jax.random.split(key)
+
+            mean_param, cov_param, neural_output_batch, cost_batch_temp\
+                 = self.cem_helper.compute_shifted_samples(key, neural_output_ellite, 
+                cost_batch, idx_ellite, mean_param, cov_param)
+        
+            idx_min = jnp.argmin(cost_batch_temp)
+          
+            res = res.at[idx].set(jnp.min(cost_batch_temp))
+            res_2 = res_2.at[idx].set(res_ellite_mmd[idx_min])
+
+            return (res,res_2,lamda_x, lamda_y, neural_output_batch,
+                    mean_param,cov_param,s_lane),\
+                (c_x_ellite_mmd[idx_min],c_y_ellite_mmd[idx_min],
+                 steer_ellite_mmd[idx_min])
+
+        carry_init = (res_init,res_2_init,lamda_x_init, lamda_y_init, \
+            neural_output_batch_init,mean_param_init, cov_param_init,s_lane_init)
+
+        carry_final,result = lax.scan(lax_cem,carry_init,jnp.arange(self.maxiter_cem))
+
+        res,res_2,lamda_x, lamda_y, neural_output_batch,\
+            mean_param,cov_param,s_lane = carry_final 
+        
+        cx_best = result[0][-1]
+        cy_best = result[1][-1]
+
+        xdot_best = jnp.dot(self.Pdot_jax, cx_best)
+        ydot_best = jnp.dot(self.Pdot_jax, cy_best)
+        v_best = jnp.sqrt(xdot_best**2+ydot_best**2)
+        steering_best = result[2][-1]
+               
+        return cx_best,cy_best,v_best,steering_best,mean_param
